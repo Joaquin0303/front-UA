93bf66684e55a9f42e528bae87eeae7aaa63f682
Joaquin0303 <jamaya@mazz.com.ar>
2023-12-20 20:14:38 -0300

    Se agrego boton de licencias

--- src/components/table/TableEmploee.jsx
+++ src/components/table/TableEmploee.jsx
@@ -4,6 +4,8 @@ import i18n from '../../localization/i18n'
 import { IoEyeSharp } from "react-icons/io5";
 import { BsFillPencilFill } from 'react-icons/bs'
 import { FaArrowDown, FaArrowUp } from "react-icons/fa";
+import { TbLicense } from "react-icons/tb";
+
 
 const TableEmployee = ({ dataList, setModal, statusActive }) => {
     return (
@@ -45,6 +47,9 @@ const TableEmployee = ({ dataList, setModal, statusActive }) => {
                                     </div>}
                                     {d.codigoEstadoEmpleado.id == 88 && <div title="Dar de Alta" onClick={(e) => { e.stopPropagation(); setModal('activate', d) }}>
                                         <FaArrowUp />
+                                    </div>}
+                                    {(d.codigoEstadoEmpleado.id == 87 || d.codigoEstadoEmpleado.id == 88) && <div title="Dar Licencia" onClick={(e) => { e.stopPropagation(); setModal('inactivate', d) }}>
+                                        <TbLicense />
                                     </div>}
                                 </span>
                             </td>
