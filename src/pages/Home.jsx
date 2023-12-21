3d73413ec479060054b79b2a89c37d0309d96209
Joaquin0303 <jamaya@mazz.com.ar>
2023-12-21 13:16:22 -0300

    Tmaño de imagen corregido

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,5 +4,5 @@ import ImgFondoHome from '../img/fondo-RRHH.jpg'
 export const Home = () => 
 <div className='bloque_principal'>
     <h1>Bienvenido al Sistema de Recursos Humanos</h1>
-    <img src={ImgFondoHome} alt="Imagen Home" />
+    <img className='imgHome' src={ImgFondoHome} alt="Imagen Home" />
 </div>
