66cc61c363400a7ac0e64d94be8750ce6a4e79dc
Joaquin0303 <jamaya@mazz.com.ar>
2023-12-26 18:04:22 -0300

    responsive image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,5 +4,7 @@ import ImgFondoHome from '../img/fondo-RRHH.jpg'
 export const Home = () => 
 <div className='bloque_principal'>
     <h1>Bienvenido al Sistema de Recursos Humanos</h1>
-    <img className='imgHome' src={ImgFondoHome} alt="Imagen Home" />
+    <div className='container-img'>
+        <img className='imgHome' src={ImgFondoHome} alt="Imagen Home" />
+    </div>
 </div>
