f02ba25adb4252410a767bdcf0fd630d7fb85fe3
Joaquin0303 <jamaya@mazz.com.ar>
2023-12-28 13:42:44 -0300

    Navegacion por links

--- src/pages/ABMPage.jsx
+++ src/pages/ABMPage.jsx
@@ -6,10 +6,23 @@ import i18n from "../localization/i18n";
 
 const ABMPage = ({ pageConfiguration, pageName, dataList, dataModel, onAdd, onEdit, onRemove, searchKey, setActive, matchHandler, statusActive }) => {
 
+    let seccion;
+    let to;
+
+    if (pageName === 'Empleados' || pageName === 'Carga de familia' || pageName === 'Empleado externo' || pageName === 'Histial de licencia' || pageName === 'Historial de puesto' || pageName === 'Ingreso caido'){
+        seccion = '/ Administracion de Empleados';
+        to = '/administracion-empleados'}
+    else if (pageName === 'Usuarios' || pageName === 'Roles' || pageName === 'Permisos'){
+        seccion = '/ Administracion de Usuarios';
+        to = '/administracion-usuarios'}
+    else if (pageName === 'Tipo de Parámetro' || pageName === 'Parametros' || pageName === 'Secuenciador' || pageName === 'Puesto' || pageName === 'Países'){
+        seccion = '/ Administracion de Parámetros';
+        to = '/administracion-parametros'}
+        
     return (
         <div className='bloque-principal'>
             <div className="bloque-secundario">
-                <Link to="/">Inicio</Link><p>/ {i18n.t(pageName)}</p>
+                <Link to="/">Inicio</Link> <Link to={to}>{seccion}</Link> <p>/ {i18n.t(pageName)}</p>
             </div>
             <TableWithSearch pageConfiguration={pageConfiguration} pageName={pageName} dataList={dataList} dataModel={dataModel} onAdd={onAdd} onEdit={onEdit} onRemove={onRemove} searchKey={searchKey} setActive={setActive} matchHandler={matchHandler} statusActive={statusActive} />
         </div>
