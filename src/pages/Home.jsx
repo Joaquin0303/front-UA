b2e81e99bf6ad17a592b67b613791464916197ff
Joaquin0303 <jamaya@mazz.com.ar>
2024-01-12 11:15:18 -0300

    cambio de titulos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import ImgFondoHome from '../img/fondo-RRHH.jpg'
 
 export const Home = () => 
 <div className='bloque_principal'>
-    <h1>Bienvenido al Sistema de Recursos Humanos</h1>
+    <h1>ABM HR System</h1>
     <div className='container-img'>
         <img className='imgHome' src={ImgFondoHome} alt="Imagen Home" />
     </div>
