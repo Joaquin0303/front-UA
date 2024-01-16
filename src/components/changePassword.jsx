ef7f97533a614d126302196a66c2bef79d7a12db
Joaquin0303 <jamaya@mazz.com.ar>
2024-01-16 16:51:49 -0300

    change Password

---
+++ src/components/changePassword.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const changePassword = () => {
+  return (
+    <>
+     <form class="change-pass">
+        <h1>Cambio de Contraseña</h1>
+        <p>Nueva Contraseña</p>
+        <input type="text" name="" placeholder="" id=""></input>
+        <p>Repita Nueva Contraseña</p>
+        <input type="text" name="" placeholder="" id=""></input>
+        <button type="submit">Confirmar</button>
+    </form>
+    </>
+  )
+}
