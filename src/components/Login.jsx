b508333e31d0db25a600aba4bbca3a9f23baf1c2
Joaquin0303 <jamaya@mazz.com.ar>
2024-01-30 18:17:18 -0300

    ChangePassword

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -65,7 +65,7 @@ const Login = ({ setToken }) => {
                 {showChangePassword ? (<ChangePassword />) : (
                     <>
                         <img src={gifWork} alt="gif-work" id='gif' />
-                        <form className='form' onSubmit={handleSubmit}>
+                        <form className='form-login' onSubmit={handleSubmit}>
                             <h2>Iniciar Sesión</h2>
                             <div className="form-group-login">
                                 <label htmlFor="login-user">Nombre de Usuario</label>
