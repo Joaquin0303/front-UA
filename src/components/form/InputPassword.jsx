83bf522685577ee12692eb37d2204be8db490196
sebastianbarrientos <sba04012@gmail.com>
2024-02-01 16:45:01 -0300

    Evento onFocus en InputPassword

--- src/components/form/InputPassword.jsx
+++ src/components/form/InputPassword.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect } from "react";
 import i18n from "../../localization/i18n";
+import PasswordRules from "../../pages/UserAdmin/PasswordRules";
 
 const InputPassword = ({ validation, name, value, updateFormData, disabled }) => {
+
+    const [showPasswordRules, setShowPasswordRules] = useState(false);
 
     useEffect(() => {
         updateFormData(name, value);
@@ -10,7 +13,8 @@ const InputPassword = ({ validation, name, value, updateFormData, disabled }) =>
     return (
         <div className='form-group'>
             <label className='label' htmlFor="id">{i18n.t(name)}</label>
-            <input disabled={disabled} type="password" name={name} value={value ? value : ''} onChange={(e) => updateFormData(name, e.target.value)} />
+            <input disabled={disabled} type="password" name={name} value={value ? value : ''} onChange={(e) => updateFormData(name, e.target.value)} onFocus={() => { setShowPasswordRules(true) }} onBlur={() => { setShowPasswordRules(false) }} />
+            {showPasswordRules && <PasswordRules />}
             {validation && validation[name] && <div className="form-field-error-msg">{validation[name]}</div>}
         </div>
     );
