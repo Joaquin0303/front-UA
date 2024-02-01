92421c0970f5b2e151f765ac51e6ad58252a8d29
sebastianbarrientos <sba04012@gmail.com>
2024-02-01 12:48:05 -0300

    Password Security fix

--- src/pages/UserAdmin/PasswordSecurity.jsx
+++ src/pages/UserAdmin/PasswordSecurity.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ABMPage from '../ABMPage';
 import { addPaswordSecurity, getPaswordSecurity, updatePaswordSecurity } from '../../services/LoginAndSecurityServices';
 import { TABLE_ACTIONS } from '../../utils/GeneralConstants';
