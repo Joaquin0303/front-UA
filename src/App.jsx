eed9f3c1be785104cfbd38f52a8be93c3add1069
gmonzon <gmonzon@mazz.com.ar>
2024-02-02 13:45:42 -0300

    fix app

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ import GenericReportPage from './pages/ReportsAdmin/GenericReportPage';
 import LicencesReportPage from './pages/ReportsAdmin/LicencesReportPage';
 import { jwtDecode } from "jwt-decode";
 import { decodeToken } from './utils/Utils';
-import { PERMISSION } from './utils/PermissionLIst';
+import { PERMISSION } from './utils/PermissionList';
 
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJ2YWx1ZSI6InNlciJ9._RCDTTuGvtXwENLAHhQvvJuoYSbkXP_JPoGv2wzoQQo";
 const decoded = jwtDecode(token);
