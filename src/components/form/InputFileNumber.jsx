d2a04b12e1d9f96ab864f0226c64503184027eb6
gmonzon <gmonzon@mazz.com.ar>
2024-02-06 13:51:47 -0300

    add employee names

--- src/components/form/InputFileNumber.jsx
+++ src/components/form/InputFileNumber.jsx
@@ -5,11 +5,15 @@ import { getEmployees } from "../../services/EmployeeServices";
 const InputFileNumber = ({ validation, name, value, updateFormData }) => {
 
     const [employeeList, setEmployeeList] = useState([]);
+    const [employeeNames, setEmployeeNames] = useState(null);
 
     const employeeSelectorChangeHandler = (e) => {
-        if (validateFileNumber(e.target.value)) {
+        const emp = getEmploeeByFileNumber(e.target.value);
+        if (emp) {
+            setEmployeeNames(emp.apellido + ' ' + emp.nombre);
             updateFormData(name, e.target.value);
         } else {
+            setEmployeeNames(null);
             updateFormData(name, null);
         }
     }
@@ -22,23 +26,26 @@ const InputFileNumber = ({ validation, name, value, updateFormData }) => {
     }, []);
 
     useEffect(() => {
-        if (employeeList.length > 0 && !validateFileNumber(value)) {
+        if (employeeList.length > 0 && !getEmploeeByFileNumber(value)) {
             updateFormData(name, null);
         }
     }, [employeeList]);
 
-    const validateFileNumber = (fileNumber) => {
-        return (employeeList.find(emp => emp.numeroLegajo == fileNumber) != null)
+    const getEmploeeByFileNumber = (fileNumber) => {
+        return employeeList.find(emp => emp.numeroLegajo == fileNumber);
     }
 
     return (
         <div className='form-group'>
             <label className='label' htmlFor="id">{i18n.t(name)}</label>
             <input list="ice-cream-flavors" type="text" name={name} value={value} onChange={employeeSelectorChangeHandler} />
+            {employeeNames && <div className='undertext'>
+                <span className='label' >Empleado: </span><span>{employeeNames}</span>
+            </div>}
             {validation && validation[name] && <div className="form-field-error-msg">{validation[name]}</div>}
             <datalist id="ice-cream-flavors">
                 {employeeList.map((emp, i) => {
-                    return <option key={i} value={emp.numeroLegajo} >{emp.nombre} {emp.apellido}</option>
+                    return <option key={i} value={emp.numeroLegajo} >{emp.apellido} {emp.apellido}</option>
                 })}
             </datalist>
         </div>
