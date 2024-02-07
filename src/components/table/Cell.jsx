1d2a85ab630cedf3dbce9f71dfaaf4f59bcf658e
gmonzon <gmonzon@mazz.com.ar>
2024-02-07 19:16:18 -0300

    fix global report

--- src/components/table/Cell.jsx
+++ src/components/table/Cell.jsx
@@ -4,7 +4,7 @@ const Cell = ({ value, labels, hidden }) => {
     if (typeof value == 'boolean')
         value = value ? 'Si' : 'No';
     return (
-        <td hidden={hidden}>{labels ? labels.find(l => l.value == value).label : value != 'null' ? value : ''}</td>
+        <td hidden={hidden}>{labels ? labels.find(l => l.value == value)?.label : value != 'null' ? value : ''}</td>
     );
 }
 
