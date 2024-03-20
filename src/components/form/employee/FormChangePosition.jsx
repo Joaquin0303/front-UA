18bb7ced0dcfe585a8a7a8deb2403bad106ebf19
gmonzon <gmonzon@mazz.com.ar>
2024-03-20 18:01:46 -0300

    fix change position

--- src/components/form/employee/FormChangePosition.jsx
+++ src/components/form/employee/FormChangePosition.jsx
@@ -9,8 +9,7 @@ import { parseInputDate, parseToday } from '../../../utils/Utils';
 
 const FormChangePosition = ({ parameterList, data, closeModal, onSubmitForm }) => {
     const [validation, setValidation] = useState();
-    const [formData, setFormData] = useState({ ...data });
-
+    const [formData, setFormData] = useState({ ...data, codigoDireccion: data.codigoPuesto.codigoDireccion });
     const updateFormData = (key, value) => {
         formData[key] = value;
         setFormData({ ...formData });
@@ -71,9 +70,9 @@ const FormChangePosition = ({ parameterList, data, closeModal, onSubmitForm }) =
                     <div className="form-field-container">
                         <InputCountry validation={validation} name="codigoPais" value={formData["codigoPais"]} updateFormData={updateFormData} />
                         <InputParameter validation={validation} name="codigoOficina" value={formData["codigoOficina"]} parameterList={parameterList.filter(p => p.tipoParametro.id == 19)} updateFormData={updateFormData} country={formData["codigoPais"]} />
-                        <InputParameter validation={validation} name="codigoDireccion" value={formData['codigoPuesto'] && formData['codigoPuesto'].codigoDireccion ? formData['codigoPuesto'].codigoDireccion : null} parameterList={parameterList.filter(p => p.tipoParametro.id == 6)} updateFormData={updateFormData} />
+                        <InputParameter validation={validation} name="codigoDireccion" value={formData['codigoDireccion']} parameterList={parameterList.filter(p => p.tipoParametro.id == 6)} updateFormData={updateFormData} />
                         <InputDate validation={validation} name="fechaInicioPuesto" value={formData["fechaInicioPuesto"]} updateFormData={updateFormData} />
-                        <InputPositionCode validation={validation} name="codigoPuesto" value={formData["codigoPuesto"]} updateFormData={updateFormData} directionCode={formData['codigoPuesto'].codigoDireccion} countryCode={formData['codigoPais']} categoryCode={formData['codigoPuesto'].codigoCategoria} currentPositionId={formData['id']} />
+                        <InputPositionCode validation={validation} name="codigoPuesto" value={formData["codigoPuesto"]} updateFormData={updateFormData} directionCode={formData['codigoDireccion']} countryCode={formData['codigoPais']} categoryCode={formData['codigoPuesto'].codigoCategoria} currentPositionId={formData['id']} />
 
                         <InputText disabled={true} validation={validation} name="Categoría" updateFormData={() => { }} value={formData['codigoPuesto'] && formData['codigoPuesto'].codigoCategoria ? formData['codigoPuesto'].codigoCategoria.descripcion : ""} />
                         <InputText disabled={true} validation={validation} name="A quien reporta" updateFormData={() => { }} value={formData['codigoPuesto'] && formData['codigoPuesto'].codigoPuestoAlQueReporta ? formData['codigoPuesto'].codigoPuestoAlQueReporta.descripcion : ""} />
